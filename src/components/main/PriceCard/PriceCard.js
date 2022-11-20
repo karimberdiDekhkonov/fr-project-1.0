a34ec9f055735ad26865874681bf024f14eb0cde
karimberdiDekhkonov <karimberdidehqonov0921@gmail.com>
2022-11-20 22:28:22 +0100

    props are used for three cards

--- src/components/main/PriceCard/PriceCard.js
+++ src/components/main/PriceCard/PriceCard.js
@@ -1,11 +1,12 @@
 import {Component} from 'react';
 class PriceCard extends Component{
     render(){
+      const {type, price, btnText} = this.props.data;
       return <div className="container main-inner-box p-0 shadow">
-      <div className="inner-top fs-3 mb-4">Free</div>
-      <div className="inner-price fs-4"> <span className='price fw-bolder fs-1'>$0</span> / mo</div>
+      <div className="inner-top fs-3 mb-4">{type}</div>
+      <div className="inner-price fs-4"> <span className='price fw-bolder fs-1'>${price}</span> / mo</div>
       <div className="inner-des px-5">10 users included 2 GB of storage Email support Help center access</div>
-      <button className='btn btn-outline-primary mt-4 inner-btn'>Sign up for free</button>
+      <button className='btn btn-outline-primary mt-4 inner-btn'>{btnText}</button>
     </div>
     }
   }
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,6 +1,24 @@
 import { Component } from "react";
 import PriceCard from "./PriceCard/PriceCard";
 
+const cardList = [
+  {
+    type: "Free",
+    price: 0,
+    btnText: "Sign up for free"
+  },
+  {
+    type: "Pro",
+    price: 15,
+    btnText: "Get started"
+  },
+  {
+    type: "Enterprise",
+    price: 29,
+    btnText: "Contact us"
+  }
+]
+
 class Main extends Component{
     render(){
       return <main className='d-flex flex-column text-center pt-5 mt-5 gap-3'>
@@ -11,9 +29,9 @@ class Main extends Component{
           Quickly build an effective pricing table for your potential customers with this Bootstrap example. It's built with default Bootstrap components and utilities with little customization.
           </div>
           <div className="main-box3 d-flex">
-            <PriceCard/>
-            <PriceCard/>
-            <PriceCard/>
+            {
+              cardList.map((value) => <PriceCard data = {value}/>)
+            }
           </div>
         </main>
     }
